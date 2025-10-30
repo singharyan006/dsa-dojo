6d4fa5389f003edd4c028951c5633dc5422efadf
singharyan006 <aryanraisingh2006@gmail.com>
2025-10-30 17:22:42 +0530

    practice: working out my understanding in trees implementation

---
+++ PRACTICE/Graphs/workout/dijkstras.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <limits.h>
+
+#define V 5
+
+int minDist(int dist[], int visited[]) {
+    int minDistance = INT_MAX, minVertex;
+    for(int vertex = 0; vertex < V; vertex++) {
+        
+    } 
+}
